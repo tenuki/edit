988c810087b8b57dbbc21bbf3f54af6d72e47036
david weil <tenuki@gmail.com>
2017-08-14 01:48:26 -0300

    updated install/package info..

--- setup.py
+++ setup.py
@@ -8,5 +8,15 @@ setup(name='edith',
       author_email='tenuki@gmail.com',
       url='https://github.com/tenuki/edith',
       py_modules=['edith'],
-      license="GNU General Public License v3.0"
+      license="GNU General Public License v3.0",
+      classifiers=[
+            'Development Status :: 3 - Alpha',
+            'Intended Audience :: Developers',
+            'Topic :: Software Development :: Language Tools',
+            'GNU General Public License v3.0',
+
+            # Specify the Python versions you support here. In particular, ensure
+            # that you indicate whether you support Python 2, Python 3 or both.
+            'Programming Language :: Python :: 2',
+            ]
      )
