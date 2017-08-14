1342c9369092c30bb6194ec4726d3e21b317160d
david weil <tenuki@gmail.com>
2017-08-14 12:08:43 -0300

    updated for first pypi version

--- setup.py
+++ setup.py
@@ -1,8 +1,8 @@
 #!/usr/bin/env python
-from distutils.core import setup
+from setuptools import setup
 
 setup(name='edith',
-      version='0.1.0',
+      version='0.1.0a1',
       description='Edit-distance implementation with edit-path retrieval',
       author='david weil (tenuki)',
       author_email='tenuki@gmail.com',
@@ -12,11 +12,6 @@ setup(name='edith',
       classifiers=[
             'Development Status :: 3 - Alpha',
             'Intended Audience :: Developers',
-            'Topic :: Software Development :: Language Tools',
-            'GNU General Public License v3.0',
-
-            # Specify the Python versions you support here. In particular, ensure
-            # that you indicate whether you support Python 2, Python 3 or both.
             'Programming Language :: Python :: 2',
             ]
      )
