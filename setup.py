be834ff39bd1c96e8764913dee3c8d96eaac5396
david weil <tenuki@gmail.com>
2017-08-14 01:29:16 -0300

    package script

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+from distutils.core import setup
+
+setup(name='edith',
+      version='0.1.0',
+      description='Edit-distance implementation with edit-path retrieval',
+      author='david weil (tenuki)',
+      author_email='tenuki@gmail.com',
+      url='https://github.com/tenuki/edith',
+      py_modules=['edith'],
+      license="GNU General Public License v3.0"
+     )
